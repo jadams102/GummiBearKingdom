69d327e0aa1484c2fc0f6d8f0b344dc4efcdf562
Joel Adams <joeladamsdesign@gmail.com>
2018-04-20 08:36:42 -0700

    add product model

---
+++ GummiBearKingdom/GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public float price { get; set; }
+    }
+}
