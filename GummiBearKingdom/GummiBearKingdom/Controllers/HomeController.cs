64e92cf381a26957c458ac7a65e41bc929b7afa4
Joel Adams <joeladamsdesign@gmail.com>
2018-04-21 17:46:47 -0700

    add  product list to landing page

--- GummiBearKingdom/GummiBearKingdom/Controllers/HomeController.cs
+++ GummiBearKingdom/GummiBearKingdom/Controllers/HomeController.cs
@@ -3,33 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GummiBearKingdom.Controllers
 {
     public class HomeController : Controller
     {
+        private GummiBearDbContext db = new GummiBearDbContext();
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            List<Product> model = db.Products.ToList();
+            return View(model);
         }
     }
 }
