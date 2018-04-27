2a3402e36bbc64ef9d889c269e8d932cf4493f06
Joel Adams <joeladamsdesign@gmail.com>
2018-04-27 08:21:08 -0700

    add review model

---
+++ GummiBearKingdom/GummiBearKingdom/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string UserName { get; set; }
+        public string Body { get; set; }
+        public int ProductId { get; set; }
+    }
+}
