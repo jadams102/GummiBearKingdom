8be292add9389a5b8c8a79078c3ad7bb09be8891
Joel Adams <joeladamsdesign@gmail.com>
2018-04-29 08:14:44 -0700

    add review getters test

--- GummiBearKingdom/GummiBearTests/Model Tests/ReviewTests.cs
+++ GummiBearKingdom/GummiBearTests/Model Tests/ReviewTests.cs
@@ -14,5 +14,20 @@ namespace GummiBearTests.Model_Tests
 
             Assert.AreEqual(testReview.UserName, "A User");
         }
+        [TestMethod]
+        public void Getters_GetReviewData_Review()
+        {
+            Review testReview = new Review { ReviewId = 1, UserName = "A User", Body = "A Body", Rating = 5 };
+
+            var idResult = testReview.ReviewId;
+            var userResult = testReview.UserName;
+            var bodyResult = testReview.Body;
+            var ratingResult = testReview.Rating;
+
+            Assert.AreEqual(idResult, 1);
+            Assert.AreEqual(userResult, "A User");
+            Assert.AreEqual(bodyResult, "A Body");
+            Assert.AreEqual(ratingResult, 5);
+        }
     }
 }
